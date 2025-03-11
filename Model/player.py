3861af432b644e5aa08a771f680ade2fcee73d9e
Joker <nikalveis80@gmail.com>
2025-03-11 10:09:29 +0100

    Tilføje playerstats og stats multiplier
    Playerstats er lavet i en klasse, hvor alle stats er i integer datatype. Så tilføjede jeg en statsmultiplier, hvor jeg bruger match statement.

--- Model/player.py
+++ Model/player.py
@@ -1,16 +1,16 @@
 import pyinputplus as pyip
 
-class playerStats:
-    def __init__(self, health, strength, defense, speed, energy, currentLife, maxLife):
-        self.health = int(health)
-        self.strength = int(strength)
-        self.defense = int(defense)
-        self.speed = int(speed)
-        self.energy = int(energy)
-        self.currentLife = int(currentLife)
-        self.maxLife = int(maxLife)
+class PlayerStats:
+    def __init__(self, health, strength, defense, speed, energy, currentLife, maxLife) -> int:
+        self.health = health
+        self.strength = strength
+        self.defense = defense
+        self.speed = speed
+        self.energy = energy
+        self.currentLife = currentLife
+        self.maxLife = maxLife
     
-    def __str__(self):
+    def __str__(self) -> str:
         return f'''¨
         Player Stats
         Health: {self.health}
@@ -22,15 +22,45 @@ class playerStats:
         Max Life: {self.maxLife}
         '''
     
-    def statsMultiplier(self):
-        response = pyip.inputStr('Choose which stats to multiply')
-        if response == 'health':
-            self.health *= 5
+    def statsMultiplier(self, value: str) -> int:
+        while True:
+            value = pyip.inputStr("Input a value from player's stats to multiply by: ")
+            value = value.lower()
+            match value:
+                case 'health':  
+                    print('Multiplying health...')
+                    self.health *= 2
+                    return f'Updated health: {self.health}' 
+                case 'strength':
+                    print('Multiplying strength...')
+                    self.strength *= 2
+                    return f'Updated strength: {self.strength}'
+                case 'defense':
+                    print('Multiplying defense...')
+                    self.defense *= 2
+                    return f'Updated defense: {self.defense}'
+                case 'speed':
+                    print('Multiplying speed...')
+                    self.speed *= 2
+                    return f'Updated speed: {self.speed}'
+                case 'energy':
+                    print('Multiplying energy...')
+                    self.energy *= 2
+                    return f'Updated energy: {self.energy}'
+                case 'currentlife':
+                    print('Multiplying current life...')
+                    self.currentLife *= 2
+                    return f'Updated current life: {self.currentLife}'
+                case 'maxlife':
+                    print('Multiplying max life...')
+                    self.maxLife *= 2
+                    return f'Updated max life: {self.maxLife}'
+                case _:
+                    print('Invalid input. Please input a valid stats.')
 
-
-beginnerStats = playerStats(10, 5, 4, 3, 20, 18, 81)
+beginnerStats: int = PlayerStats(10, 5, 4, 3, 20, 18, 81)
 print(beginnerStats)
 
-playerStats.statsMultiplier(beginnerStats)
-currentStats = beginnerStats
-print(currentStats)
+currentStats: str = beginnerStats.statsMultiplier(value='')
+
+print(beginnerStats)
