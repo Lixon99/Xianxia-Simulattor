6e985d44c39cb55e299142241cd148622b98b73f
Fyzle <gabriel.liborio.madsen@gmail.com>
2025-03-25 14:02:40 +0100

    Game page implementering
    Jeg har implementeret knapper og lagt det ind på game_page, herunder skal knapperne føre til fight page og cultivate page. Men det laver jeg i morgen.

--- View/Pages.py
+++ View/Pages.py
@@ -21,21 +21,32 @@ def main_page(screen):
                     return "game"
     
 def game_page(screen):
-    gameBackground = pygame.image.load('')
+    gameBackground = pygame.image.load('GamePage.png')
+    CultivateButton = pygame.image.load('CultivateButton.png')
+    FightButton = pygame.image.load('FightButton.png')
 
-    font = pygame.font.Font(None, 48)
-    text = font.render("game page testning", True, (255, 255, 255))
-    textpos = text.get_rect(center=(640, 360))
+    CultivateButtonPos = CultivateButton.get_rect(center=(244, 592))
+    FightButtonPos = FightButton.get_rect(center=(640, 180))
     
+    font = pygame.font.Font(None, 48)
+    text = font.render("game page", True, (255, 255, 255))
+    textpos = text.get_rect(center=(640, 360))
+
     while True:
-        screen.fill((255, 255, 255))
-        screen.blut(text, textpos)
+        screen.blit(gameBackground, (0, 0))
+        screen.blit(text, textpos)
+        screen.blit(CultivateButton, CultivateButtonPos)
+        screen.blit(FightButton, FightButtonPos)
         pygame.display.flip()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return None
-
-
-    
+            
+def fight_page(screen):
+    pass
+    # Skal lave fight page et andet tidspunkt
+def cultivate_page(screen):
+    pass
+    # Skal lave cultivate page på et andet tidspunkt
